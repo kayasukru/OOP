3ba8bf67574b2cef0191b12fe21dc4e01202d5f4
Sukru Kaya <kayasukru@gmail.com>
2024-06-21 18:53:03 +0300

    v0.2.0-Inheraitance-override. Kalıtım alınan sınıftan alınan metodlar override edildiğinde, miras alınan sınıftaki metodlara virtual özelliği eklenmelidir.

---
+++ OOP.Inheritance/Rectangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Inheritance
+{
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Rectangle drawing task...");
+            //base.Draw();
+        }
+    }
+}
