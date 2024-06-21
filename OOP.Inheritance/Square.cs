3981bb5b384efdc0c2362887adc279cc85ea8b4c
Sukru Kaya <kayasukru@gmail.com>
2024-06-21 19:18:26 +0300

    v0.2.3- Miras alınan sınıftaki metodla aynı isimli metod varsa ve miras alandaki metod kullanılacaksa, miras alan sınıftaki metoda new kelimesi eklenir.

--- OOP.Inheritance/Square.cs
+++ OOP.Inheritance/Square.cs
@@ -8,5 +8,10 @@ namespace OOP.Inheritance
 {
     public class Square : Rectangle
     {
+        new public void Calculate()
+        {
+            System.Console.WriteLine("Calculated by Square...");
+        }
+
     }
 }
