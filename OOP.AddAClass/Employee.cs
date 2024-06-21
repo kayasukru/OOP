20fa5b72623ad2502baa57e159426871a0896ad5
Sukru Kaya <kayasukru@gmail.com>
2024-06-21 17:16:50 +0300

    v0.1.0-YeniProjeYeniSinif

---
+++ OOP.AddAClass/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.AddAClass
+{
+    internal class Employee
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
+
+        public int Age { get; set; }
+    }
+}
---
+++ OOP.AddAClass/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("-------- Standart Sınıf Yapısı ile kullanım ---------");
+        // standart sınıf yapısı ile kullanım
+        Employee employee = new Employee();
+        employee.FirstName = "Şükrü";
+        employee.LastName = "Kaya";
+        employee.Age = 57;
+        Console.WriteLine(employee.FirstName + " " + employee.LastName + " " + employee.Age);
+        Console.WriteLine();
+    }
+}
