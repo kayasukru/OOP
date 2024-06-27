2fe7dbe1f4e1ec2a3f9a5786e1aacdf4d8a607c2
Sukru Kaya <kayasukru@gmail.com>
2024-06-27 19:18:29 +0300

    v0.6.6-Interface-IEnumerable

---
+++ OOP.Interface-IEnumerable_App/Dizi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Interface_IEnumerable_App
+{
+    public class Dizi:IEnumerable
+    {
+        private int[] InnerList { get; set; }
+        private int index = 0;
+
+        public Dizi(int size = 16)
+        {
+            InnerList = new int[size];
+        }
+
+        public Dizi(params int[] initial)
+        {
+            InnerList = new int[initial.Length];
+            foreach (var item in initial)
+            {
+                InnerList[index] = item;
+                index++;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return InnerList.Take(index).GetEnumerator();
+        }
+    }
+}
