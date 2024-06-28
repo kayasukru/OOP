bf98d44c515e891e2db9d398136f1a79981cc2f6
Sukru Kaya <kayasukru@gmail.com>
2024-06-28 12:33:00 +0300

    v0.7.3-GenericAndStruct

---
+++ OOP.GenericAndStruct/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var stack = new Stack<Company>();
+        stack.Push(new Company()
+        {
+            Id = 1,
+            Name = "Aselsan",
+            Country="Türkiye"
+        });
+
+        Console.WriteLine(stack.Peek().Name);
+    }
+}
